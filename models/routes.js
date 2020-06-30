0ae6579b777ed7945e17451f2b809d3d3ad9ad18
Crislaine <48528703+Crislainecsoares@users.noreply.github.com>
2020-06-29 23:50:38 -0300

    remover comentários

--- models/routes.js
+++ models/routes.js
@@ -1,19 +1,5 @@
 const express = require ('express');
 const router = express.Router();
-// const campusController = require ('./campusController')
-// const cadastroController = require ('./cadastroController');
-
-// router.get('/', all)
-// router.get('/:id' , findById)
-// router.post('/', save)
-// router.put('/:id', save)
-// router.delete('/:id', remove)
-
-// router.get('/', cadastroController.all);
-// router.post('/', cadastroController.save);
-
-// router.get('/', campusController.all);
-// router.post('/', campusController.save);
 
 
 module.exports = router;
